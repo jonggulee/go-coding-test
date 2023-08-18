fdb9ca9f21e0b806c8896e430dd9f5686de40964
Jonggu Lee <jg.jake.lee@gmail.com>
2023-08-18 14:49:56 +0900

    feat: Add 문자열 섞기

--- lv0/문자열 섞기.go
+++ lv0/문자열 섞기.go
@@ -1,19 +1,7 @@
-package main
-
-import (
-	"fmt"
-)
-
 func solution(str1 string, str2 string) string {
 	var r string
 	for i := 0; i < len(str1); i++ {
 		r = r + string(str1[i]) + string(str2[i])
 	}
 	return r
-}
-
-func main() {
-	r := solution("aaaaa", "bbbbb")
-	fmt.Println(r)
-
 }
