6e1d320f39440b3b2b84ceb76f5f73ab8dfeb767
Jonggu Lee <jg.jake.lee@gmail.com>
2023-08-28 17:52:27 +0900

    feat: Add 접미사 배열

---
+++ lv0/접미사 배열.go
@@ -0,0 +1,12 @@
+import "sort"
+
+func solution(my_string string) []string {
+	r := []string{}
+	for i := 0; i < len(my_string); i++ {
+		// fmt.Println(string(my_string[i:]))
+		r = append(r, string(my_string[i:]))
+	}
+	sort.Strings(r)
+
+	return r
+}
--- main.go
+++ main.go
@@ -2,22 +2,26 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
-func solution(my_strings []string, parts [][]int) string {
-	var ret string
-	for i, str := range my_strings {
-		ret = ret + str[parts[i][0]:parts[i][1]+1]
+func solution(my_string string) []string {
+	r := []string{}
+	for i := 0; i < len(my_string); i++ {
+		// fmt.Println(string(my_string[i:]))
+		r = append(r, string(my_string[i:]))
 	}
-	return ret
+	sort.Strings(r)
+
+	return r
 }
 
 func main() {
-	r := solution([]string{"progressive", "hamburger", "hammer", "ahocorasick"}, [][]int{{0, 4}, {1, 2}, {3, 5}, {7, 7}})
+	r := solution("banana")
 	fmt.Println(r)
 
-	// r = solution([]int{1, 2, 3}, []bool{true, true, true})
-	// fmt.Println(r)
+	r = solution("programmers")
+	fmt.Println(r)
 
 	// r = solution([]int{6, 1, 5, 2, 3, 4}, []bool{true, false, true, false, false, true})
 	// fmt.Println(r)
