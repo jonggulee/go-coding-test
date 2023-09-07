34c182849ff1d4802cd18d98ff81951b8c84c68e
Jonggu Lee <jg.jake.lee@gmail.com>
2023-09-07 23:28:30 +0900

    feat: Add 문자 개수 세기

--- main.go
+++ main.go
@@ -4,22 +4,16 @@ import (
 	"fmt"
 )
 
-func solution(q int, r int, code string) string {
-	ret := ""
-	for i := 0; i < len(code); i++ {
-		if i%q == r {
-			ret += string(code[i])
-		}
-	}
-	return ret
+func solution(my_string string) []int {
+	return []int{}
 }
 
 func main() {
-	r := solution(3, 1, "qjnwezgrpirldywt")
+	r := solution("Programmers")
 	fmt.Println(r)
 
-	r = solution(1, 0, "programmers")
-	fmt.Println(r)
+	// r = solution(1, 0, "programmers")
+	// fmt.Println(r)
 
 	// r = solution(-4, 2)
 	// fmt.Println(r)
