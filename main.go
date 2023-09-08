1cd38d0bf6a1b8cb4b241bc98fff6c137968574f
Jonggu Lee <jg.jake.lee@gmail.com>
2023-09-08 23:55:56 +0900

    feat: Add 글자지우기

--- main.go
+++ main.go
@@ -4,20 +4,16 @@ import (
 	"fmt"
 )
 
-func solution(n int, k int) []int {
-	r := []int{}
-	for i := k; i <= n; i += k {
-		r = append(r, i)
-	}
-	return r
+func solution(my_string string, indices []int) string {
+	return ""
 }
 
 func main() {
-	r := solution(10, 3)
+	r := solution("apporoograpemmemprs", []int{1, 16, 6, 15, 0, 10, 11, 3})
 	fmt.Println(r)
 
-	r = solution(15, 5)
-	fmt.Println(r)
+	// r = solution(15, 5)
+	// fmt.Println(r)
 
 	// r = solution(-4, 2)
 	// fmt.Println(r)
