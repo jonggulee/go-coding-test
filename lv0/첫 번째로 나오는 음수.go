24b4ac7085245602e5eeec971026e278d5f2e7f9
Jonggu Lee <jg.jake.lee@gmail.com>
2023-09-11 12:02:41 +0900

    feat: Add 첫 번째로 나오는 음수

---
+++ lv0/첫 번째로 나오는 음수.go
@@ -0,0 +1,8 @@
+func solution(num_list []int) int {
+	for i, v := range num_list {
+		if v < 0 {
+			return i
+		}
+	}
+	return -1
+}
--- main.go
+++ main.go
@@ -4,30 +4,21 @@ import (
 	"fmt"
 )
 
-func solution(n int, slicer []int, num_list []int) []int {
-	switch n {
-	case 1:
-		return num_list[:slicer[1]+1]
-	case 2:
-		return num_list[slicer[0]:]
-	case 3:
-		return num_list[slicer[0] : slicer[1]+1]
-	case 4:
-		ret := []int{}
-		for i := slicer[0]; i <= slicer[1]; i = i + slicer[2] {
-			ret = append(ret, num_list[i])
+func solution(num_list []int) int {
+	for i, v := range num_list {
+		if v < 0 {
+			return i
 		}
-		return ret
 	}
-	return []int{}
+	return -1
 }
 
 func main() {
-	r := solution(3, []int{1, 5, 2}, []int{1, 2, 3, 4, 5, 6, 7, 8, 9})
+	r := solution([]int{12, 4, 15, 46, 38, -2, 15})
 	fmt.Println(r)
 
-	r = solution(4, []int{1, 5, 2}, []int{1, 2, 3, 4, 5, 6, 7, 8, 9})
-	fmt.Println(r)
+	// r = solution(4, []int{1, 5, 2}, []int{1, 2, 3, 4, 5, 6, 7, 8, 9})
+	// fmt.Println(r)
 
 	// r = solution(24, 24)
 	// fmt.Println(r)
