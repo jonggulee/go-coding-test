ae6343a854990ab06915b1996c16de0a004cf955
Jonggu Lee <jg.jake.lee@gmail.com>
2023-09-13 14:01:22 +0900

    feat: Add n 번째 원소부터

--- main.go
+++ main.go
@@ -4,36 +4,20 @@ import (
 	"fmt"
 )
 
-func solution(arr []int) []int {
-	firstIndex := -1
-	lastIndex := -1
-
-	for i, v := range arr {
-		if v == 2 {
-			if firstIndex == -1 {
-				firstIndex = i
-			}
-			lastIndex = i
-		}
-	}
-
-	if lastIndex == -1 {
-		return []int{-1}
-	}
-
-	return arr[firstIndex : lastIndex+1]
+func solution(num_list []int, n int) []int {
+	return num_list[n-1:]
 }
 
 func main() {
-	r := solution([]int{1, 2, 1, 4, 5, 2, 9})
+	r := solution([]int{2, 1, 6}, 3)
 	fmt.Println(r)
 
-	r = solution([]int{1, 2, 1})
+	r = solution([]int{5, 2, 1, 7, 5}, 2)
 	fmt.Println(r)
 
-	r = solution([]int{1, 1, 1})
-	fmt.Println(r)
+	// r = solution([]int{1, 1, 1})
+	// fmt.Println(r)
 
-	r = solution([]int{1, 2, 1, 2, 1, 10, 2, 1})
-	fmt.Println(r)
+	// r = solution([]int{1, 2, 1, 2, 1, 10, 2, 1})
+	// fmt.Println(r)
 }
