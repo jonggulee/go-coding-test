55e7d4b46deb250f9c23f36feedc724891de724b
Jonggu Lee <jg.jake.lee@gmail.com>
2023-10-15 23:55:45 +0900

    feat: Add 두 수의 합

--- lv0/두 수의 합.go
+++ lv0/두 수의 합.go
@@ -1,12 +1,6 @@
-package main
-
-import (
-	"fmt"
-	"strconv"
-)
+import "strconv"
 
 func solution(a string, b string) string {
-	// 문자열 뒤집기
 	reverse := func(s string) string {
 		r := []rune(s)
 		for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
@@ -15,7 +9,6 @@ func solution(a string, b string) string {
 		return string(r)
 	}
 
-	// 뒤집어진 문자열로 덧셈
 	a, b = reverse(a), reverse(b)
 	lenA, lenB := len(a), len(b)
 	carry := 0
@@ -34,18 +27,4 @@ func solution(a string, b string) string {
 	}
 
 	return reverse(result)
-}
-
-func main() {
-	r := solution("582", "734")
-	fmt.Println(r)
-
-	// r = solution(2573)
-	// fmt.Println(r)
-
-	// r = solution("40000 * 40000")
-	// fmt.Println(r)
-
-	// r = solution([]int{1, 2, 1, 2, 1, 10, 2, 1})
-	// fmt.Println(r)
 }
