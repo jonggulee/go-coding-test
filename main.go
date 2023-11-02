1b8da755dbc06367fecdac7d9f24a7062acccee3
Jonggu Lee <jg.jake.lee@gmail.com>
2023-11-02 17:55:05 +0900

    Add 날짜 비교하기

--- main.go
+++ main.go
@@ -2,28 +2,28 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-func solution(a int, b int) int {
-	if a%2 == 1 && b%2 == 1 {
-		return (a * a) + (b * b)
-	} else if a%2 == 1 && b%2 == 0 || a%2 == 0 && b%2 == 1 {
-		return 2 * (a + b)
-	} else {
-		return int(math.Abs(float64(a - b)))
+func solution(date1 []int, date2 []int) int {
+	for i := 0; i < 3; i++ {
+		if date1[i] < date2[i] {
+			return 1
+		} else if date1[i] > date2[i] {
+			return 0
+		}
 	}
+	return 0
 }
 
 func main() {
-	r := solution(3, 5)
+	r := solution([]int{2021, 12, 28}, []int{2021, 12, 29})
 	fmt.Println(r)
 
-	r = solution(6, 1)
+	r = solution([]int{1024, 10, 24}, []int{1024, 10, 24})
 	fmt.Println(r)
 
-	r = solution(2, 4)
-	fmt.Println(r)
+	// r = solution(2, 4)
+	// fmt.Println(r)
 
 	// r = solution([]int{1, 2, 1, 2, 1, 10, 2, 1})
 	// fmt.Println(r)
