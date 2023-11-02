a95eab6b54dc420be928040b786f9bc4d4fef8ef
Jonggu Lee <jg.jake.lee@gmail.com>
2023-11-02 18:06:02 +0900

    Add 커피 심부름

--- main.go
+++ main.go
@@ -4,22 +4,28 @@ import (
 	"fmt"
 )
 
-func solution(date1 []int, date2 []int) int {
-	for i := 0; i < 3; i++ {
-		if date1[i] < date2[i] {
-			return 1
-		} else if date1[i] > date2[i] {
-			return 0
+func solution(order []string) int {
+	sum := 0
+	for _, v := range order {
+		switch v {
+		case "americano", "americanoice", "iceamericano", "anything":
+			sum += 4500
+		case "hotamericano", "americanohot":
+			sum += 4500
+		case "cafelatteice", "icecafelatte", "cafelatte":
+			sum += 5000
+		case "hotcafelatte", "cafelattehot":
+			sum += 5000
 		}
 	}
-	return 0
+	return sum
 }
 
 func main() {
-	r := solution([]int{2021, 12, 28}, []int{2021, 12, 29})
+	r := solution([]string{"cafelatte", "americanoice", "hotcafelatte", "anything"})
 	fmt.Println(r)
 
-	r = solution([]int{1024, 10, 24}, []int{1024, 10, 24})
+	r = solution([]string{"americanoice", "americano", "iceamericano"})
 	fmt.Println(r)
 
 	// r = solution(2, 4)
