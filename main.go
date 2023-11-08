5309cc2020e6206967a428a5b2ebd885223b3140
Jonggu Lee <jg.jake.lee@gmail.com>
2023-11-08 23:57:17 +0900

    feat: Add 숫자 찾기

--- main.go
+++ main.go
@@ -4,20 +4,17 @@ import (
 	"fmt"
 )
 
-func solution(board [][]int, k int) int {
-	res := 0
-	for i := 0; i < len(board); i++ {
-		for j := 0; j < len(board[i]); j++ {
-			if i+j <= k {
-				res += board[i][j]
-			}
+func solution(num int, k int) int {
+	for i := 0; i < k; i++ {
+		if num[i] == k {
+			return i
 		}
 	}
-	return res
+	return -1
 }
 
 func main() {
-	r := solution([][]int{{0, 1, 2}, {1, 2, 3}, {2, 3, 4}, {3, 4, 5}}, 2)
+	r := solution(29183, 1)
 	fmt.Println(r)
 
 	// r = solution([][]int{{57, 192, 534, 2}, {9, 345, 192, 999}})
